4ccdf2cdb42a2ddc3a9d824acd6c4af1d95443b6
wwj <1454715848@qq.com>
2018-12-07 19:39:32 +0800

    fix: 添加表单验证

--- angular-user-form/src/app/form.ts
+++ angular-user-form/src/app/form.ts
@@ -2,6 +2,6 @@ export class Form {
     public name: string;
     public sex: string;
     public age: number;
-    public phone: number;
+    public phone: number|'';
     public adress: string;
 }
